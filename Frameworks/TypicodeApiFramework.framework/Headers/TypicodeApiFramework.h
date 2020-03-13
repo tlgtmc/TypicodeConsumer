6ae6a2ba81def2ebd285c38ba5f4a99313a06997
Tolga Atmaca <tlg.tmc@gmail.com>
2020-03-13 09:31:40 +0300

    l

---
+++ Frameworks/TypicodeApiFramework.framework/Headers/TypicodeApiFramework.h
@@ -0,0 +1,19 @@
+//
+//  TypicodeApiFramework.h
+//  TypicodeApiFramework
+//
+//  Created by Tolga Atmaca on 12.03.2020.
+//  Copyright © 2020 Tolga Atmaca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TypicodeApiFramework.
+FOUNDATION_EXPORT double TypicodeApiFrameworkVersionNumber;
+
+//! Project version string for TypicodeApiFramework.
+FOUNDATION_EXPORT const unsigned char TypicodeApiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypicodeApiFramework/PublicHeader.h>
+
+
